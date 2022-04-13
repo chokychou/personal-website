2f848bf96c65edec5b0f2e9b85bf993f7f500f27
chokyzhou <nobreakfast@foxmail.com>
2022-04-13 01:58:56 -0700

    added svg

--- js/scripts.js
+++ js/scripts.js
@@ -22,13 +22,16 @@ function window_check() {
     }, true);
 
     function helper () {
-        if (window.innerWidth < 440) {
+        if (window.innerWidth <= 480) {
             $('.headshot > p').removeClass("rotated-text-upright rotated-text-mixed");
             $('#main').removeClass("main-layout").addClass("main-layout-small");
+            $('.intro-text').removeClass("intro-text-big");
+
         } else {
             $('.headshot > .name-cn').addClass("rotated-text-upright");
             $('.headshot > .name-en').addClass("rotated-text-mixed");
             $('#main').removeClass("main-layout-small").addClass("main-layout");
+            $('.intro-text').addClass("intro-text-big");
         }
     }
 
