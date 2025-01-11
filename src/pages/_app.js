8860951d469ce220e3c1afcef21f71a4319015c0
chokychou <nobreakfast@foxmail.com>
2025-01-11 15:04:11 -0800

    temp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import { Head } from "next/document";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return
+  <>
+    <Head>
+      <title>Yi Zhou</title>
+      <meta
+        name="description"
+        content="Yi Zhou's personal website, made in Project IDX."
+      />
+    </Head>
+    <Component {...pageProps} />;
+  </>
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Yi Zhou</title>
-        <meta
-          name="description"
-          content="Yi Zhou's personal website, made in Project IDX."
-        />
-      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
