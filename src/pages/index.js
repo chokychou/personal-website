36d7d6f5a274ace60b99707b07fb7a84b25b27c8
chokychou <nobreakfast@foxmail.com>
2025-01-11 23:45:09 -0800

    add skeleton for gym env`

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,19 +44,19 @@ export default function Home() {
           <div className="flex gap-4 items-center flex-col sm:flex-row">
             <a
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-40"
-              href="/"
+              href="/examples/flappy-bird"
               target="_blank"
               rel="noopener noreferrer"
             >
-              More
+              See examples
             </a>
             <a
               className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-              href="https://idx.dev/"
+              href="https://www.linkedin.com/in/yi-zhou-0614b9128/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              About this site
+              Contact
             </a>
           </div>
         </main>
