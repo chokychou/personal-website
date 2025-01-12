0a46ccbd25b6457ed64dd53d044c805201fdb3b6
chokychou <nobreakfast@foxmail.com>
2025-01-11 16:27:12 -0800

    move title to client side to properly render

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Yi Zhou</title>
         <meta
           name="description"
           content="Yi Zhou's personal website, made in Project IDX."
