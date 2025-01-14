6b9079c0fe995cf132b54998361fe4efc1722d1e
chokychou <nobreakfast@foxmail.com>
2025-01-14 01:05:37 -0800

    add linkedin logo

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -15,12 +15,14 @@ export default function Example() {
   return (
     <>
       <Head>
-        <title>Reinsforcement learning on flappy bird demo</title>
+        <title>Examples</title>
       </Head>
       <div
         className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
       >
-        {/* TODO: Env goes here */}
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          {/* TODO: Env goes here */}
+        </main>
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Yi Zhou</title>
+        <title>Front Page</title>
       </Head>
       <div
         className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
@@ -37,30 +37,37 @@ export default function Home() {
             <li>
               Current employer: {" "}
               <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                Google
+                Google, LA
               </code>
             </li>
           </ol>
           <div className="flex gap-4 items-center flex-col sm:flex-row">
             <a
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-40"
-              href="/examples/flappy-bird"
+              href="/examples"
               target="_blank"
               rel="noopener noreferrer"
             >
-              See examples
-            </a>
-            <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-              href="https://www.linkedin.com/in/yi-zhou-0614b9128/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Contact
+              See ML demo
             </a>
           </div>
         </main>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://www.linkedin.com/in/yi-zhou-0614b9128/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/linkedin-mark.svg"
+              alt="chokychou yi zhou linkedin"
+              width={16}
+              height={16}
+            />
+            Contact
+          </a>
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
             href="https://github.com/chokychou"
@@ -70,7 +77,7 @@ export default function Home() {
             <Image
               aria-hidden
               src="/github-mark-white.svg"
-              alt="Globe icon"
+              alt="github yi zhou chokychou repo"
               width={16}
               height={16}
             />
